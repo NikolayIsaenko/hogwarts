bc421e6b447e6da8c40bbfb1f2807d350b093952
Николай <kolya-91@list.ru>
2024-08-20 15:10:56 +0500

    swagger and postman is work)

---
+++ src/main/java/ru/hogwarts/school/controller/FacultyController.java
@@ -0,0 +1,44 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.service.FacultyService;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("faculties")
+public class FacultyController {
+    private final FacultyService facultyService;
+
+    public FacultyController(FacultyService facultyService) {
+        this.facultyService = facultyService;
+    }
+
+    @PostMapping
+    public Faculty create(@RequestBody Faculty faculty) {
+        return facultyService.create(faculty);
+    }
+
+    @GetMapping("{id}")
+    public Faculty read(@PathVariable Long id) {
+        return facultyService.read(id);
+    }
+
+    @PutMapping("{id}")
+    public Faculty update(@PathVariable Long id, @RequestBody Faculty faculty) {
+        return facultyService.update(id, faculty);
+    }
+
+    @DeleteMapping("{id}")
+    public Faculty delete(@PathVariable Long id) {
+        return facultyService.delete(id);
+    }
+
+    @GetMapping
+    public List<Faculty> filterByAge(@RequestParam String color) {
+        return facultyService.filterByColor(color);
+    }
+
+}
