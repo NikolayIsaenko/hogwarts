adcaa856f9a9720b08e675e44897b2277b39d785
Николай <kolya-91@list.ru>
2024-09-27 11:48:36 +0500

    added log's and port(8081,8082)

--- src/main/java/ru/hogwarts/school/service/impl/FacultyServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/impl/FacultyServiceImpl.java
@@ -5,12 +5,15 @@ import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.repasitory.FacultyRepository;
 import ru.hogwarts.school.service.FacultyService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class FacultyServiceImpl implements FacultyService {
+    private static final Logger logger = LogManager.getLogger(FacultyServiceImpl.class);
     private final FacultyRepository facultyRepository;
 
     public FacultyServiceImpl(FacultyRepository facultyRepository) {
@@ -19,16 +22,19 @@ public class FacultyServiceImpl implements FacultyService {
 
     @Override
     public Faculty create(Faculty faculty) {
+        logger.info("Вызван метод для создания факультета");
         return facultyRepository.save(faculty);
     }
 
     @Override
     public Faculty read(Long id) {
+        logger.info("Вызван метод для чтения факультета с id: {}", id);
         return facultyRepository.findById(id).orElse(null);
     }
 
     @Override
     public Faculty update(Long id, Faculty faculty) {
+        logger.info("Вызван метод для обновления факультета с id: {}", id);
         return facultyRepository.findById(id).map(facultyFromDb -> {
             facultyFromDb.setName(faculty.getName());
             facultyFromDb.setColor(faculty.getColor());
@@ -39,6 +45,7 @@ public class FacultyServiceImpl implements FacultyService {
 
     @Override
     public Faculty delete(Long id) {
+        logger.info("Вызван метод для удаления факультета с id: {}", id);
         return facultyRepository.findById(id).map(faculty -> {
             facultyRepository.deleteById(id);
             return faculty;
@@ -47,11 +54,13 @@ public class FacultyServiceImpl implements FacultyService {
 
     @Override
     public List<Faculty> filterByColor(String color) {
+        logger.info("Вызван метод для фильтрации факультетов по цвету: {}", color);
         return facultyRepository.findAlByColor(color);
     }
 
     @Override
     public List<Faculty> findByColorOrNameIgnoreCase(String name, String color) {
+        logger.info("Вызван метод для поиска факультетов по цвету или имени (без учета регистра): {} или {}", name, color);
         return facultyRepository.findAll().stream()
                 .filter(faculty -> faculty.getName().equalsIgnoreCase(name) || faculty.getColor().equalsIgnoreCase(color))
                 .collect(Collectors.toList());
@@ -59,8 +68,7 @@ public class FacultyServiceImpl implements FacultyService {
 
     @Override
     public List<Student> getStudents(Long facultyId) {
-        return facultyRepository.findById(facultyId).map(faculty -> {
-            return faculty.getStudents();
-        }).orElse(null);
+        logger.info("Вызван метод для получения студентов факультета с id: {}", facultyId);
+        return facultyRepository.findById(facultyId).map(Faculty::getStudents).orElse(null);
     }
 }
--- src/main/java/ru/hogwarts/school/service/impl/StudentServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/impl/StudentServiceImpl.java
@@ -1,15 +1,18 @@
 package ru.hogwarts.school.service.impl;
 
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.repasitory.StudentRepository;
 import ru.hogwarts.school.service.StudentService;
+import org.apache.logging.log4j.LogManager;
 
 import java.util.List;
 
 @Service
 public class StudentServiceImpl implements StudentService {
+    private static final Logger logger = LogManager.getLogger(StudentServiceImpl.class);
     private final StudentRepository studentRepository;
 
     public StudentServiceImpl(StudentRepository studentRepository) {
@@ -18,16 +21,19 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public Student create(Student student) {
+        logger.info("Метод, для создания студента");
         return studentRepository.save(student);
     }
 
     @Override
     public Student read(Long id) {
+        logger.info("Метод, для получения студента");
         return studentRepository.findById(id).orElse(null);
     }
 
     @Override
     public Student update(Long id, Student student) {
+        logger.info("Метод, для обновления студента");
         return studentRepository.findById(id).map(studentFromDb -> {
             studentFromDb.setName(student.getName());
             studentFromDb.setAge(student.getAge());
@@ -38,6 +44,7 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public Student delete(Long id) {
+        logger.info("Метод, для удаления студента");
         return studentRepository.findById(id).map(student -> {
             studentRepository.deleteById(id);
             return student;
@@ -46,16 +53,19 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public List<Student> filterByAge(int age) {
+        logger.info("Метод, для фильтрации студента по возрасту");
         return studentRepository.findAllByAge(age);
     }
 
     @Override
     public List<Student> findAllByAgeBetween(int fromAge, int toAge) {
+        logger.info("Метод, для поиска студента по возрасту");
         return studentRepository.findAllByAgeBetween(fromAge, toAge);
     }
 
     @Override
     public Faculty getFacultyStudent(Long studentId) {
+        logger.info("Метод, для поиска студента по факультету");
         return studentRepository.findById(studentId)
                 .map(Student::getFaculty).orElse(null);
 
@@ -63,16 +73,19 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public List<Student> findTop5Students() {
+        logger.info("Метод, для поиска ТОП5 студентов");
         return studentRepository.findTop5Students();
     }
 
     @Override
     public Double getAverageAgeOfStudents() {
+        logger.info("Метод, для получения среднего возраста студентов");
         return studentRepository.getAverageAgeOfStudents();
     }
 
     @Override
     public long countAllStudents() {
+        logger.info("Метод, для получения всех студентов");
         return studentRepository.countAllStudents();
     }
 }
