ad885c0c44ddadfa0b6d2b069943b0bfd9d8c4ab
Николай <kolya-91@list.ru>
2024-10-02 10:04:16 +0500

    fixed

--- scripts421.sql
+++ scripts421.sql
@@ -1,20 +1,12 @@
 ALTER TABLE Student
-ADD CONSTRAINT chk_student_age CHECK (age >= 11),
-ADD CONSTRAINT uq_student_name UNIQUE (name);
+ADD CONSTRAINT chk_student_age CHECK (age >= 16);
+
+ALTER TABLE Student
+ADD CONSTRAINT uq_student_name UNIQUE (name),
+ALTER COLUMN name SET NOT NULL;
 
 ALTER TABLE Faculty
 ADD CONSTRAINT uq_faculty_name_color UNIQUE (name, color);
 
-CREATE OR REPLACE FUNCTION set_default_age() RETURNS TRIGGER AS $$
-BEGIN
-    IF NEW.age IS NULL THEN
-        NEW.age := 20;
-    END IF;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER trg_set_default_age
-BEFORE INSERT ON Student
-FOR EACH ROW
-EXECUTE FUNCTION set_default_age();
+ALTER TABLE Student
+ALTER COLUMN age SET DEFAULT 20;
